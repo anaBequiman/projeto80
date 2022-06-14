cf2f07ce3c4e78272c6a8f2f289a1fd8a22aef00
Ana <101293323+anaBequiman@users.noreply.github.com>
2022-06-14 18:58:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+nomeConvidados = [];
+function enviar(){
+        var valor = document.getElementById("nomesCaixa1").value;
+        nomeConvidados.push("<h4>" + valor + "</h4>");
+}
+function mostrar(){
+    organizacao = [];
+    for (var i=0; i<nomeConvidados.length; i++){
+        organizacao.push("<h4>" + nomeConvidados[i] + "</h4>");
+        nomeConvidados[i];
+    }
+    var removerVirgula = nomeConvidados.join(" ");
+    document.getElementById("nomes2").innerHTML= removerVirgula;
+}
+function organizada(){
+    var ordem = document.getElementById("nomes").value;
+    var pesquisa = 0;
+    var i;
+    for(i=0; i < nomeConvidados.length; i++){
+        if(s==nomeConvidados[i]){
+            pesquisa=pesquisa+1;
+        }
+    }
+    document.getElementById("nomesOrdem").innerHTML="Nome encontrado"+pesquisa+"vez(es)";
+    console.log("Nomes encontrado"+pesquisa+"vez(es)");
+}
