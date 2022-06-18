d52027e0c887f97f0d920ae087045762af11b7b9
Ana <101293323+anaBequiman@users.noreply.github.com>
2022-06-18 16:15:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 nomeConvidados = [];
 function enviar(){
         var valor = document.getElementById("nomesCaixa1").value;
-        nomeConvidados.push("<h4>" + valor + "</h4>");
+        nomeConvidados.push(valor);
 }
 function mostrar(){
     organizacao = [];
@@ -9,28 +9,29 @@ function mostrar(){
         organizacao.push("<h4>" + nomeConvidados[i] + "</h4>");
         nomeConvidados[i];
     }
-    var removerVirgula = nomeConvidados.join(" ");
+    var removerVirgula = organizacao.join(" ");
     document.getElementById("nomes2").innerHTML= removerVirgula;
 }
 function organizada(){
     organizacao = [];
+    nomeConvidados.sort();
     for (var i=0; i<nomeConvidados.length; i++){
         organizacao.push("<h4>" + nomeConvidados[i] + "</h4>");
         nomeConvidados[i];
     }
-    var removerVirgula = nomeConvidados.join(" ");
+    var removerVirgula = organizacao.join(" ");
     document.getElementById("nomes2").innerHTML= removerVirgula;
 }
 function procurar(){
-    var ordem = document.getElementById("nomes").value;
+    var s = document.getElementById("nomesCaixa2").value;
     var pesquisa = 0;
     var i;
-    var s = 0;
     for(i=0; i < nomeConvidados.length; i++){
+        console.log(nomeConvidados[i])
         if(s==nomeConvidados[i]){
             pesquisa=pesquisa+1;
         }
     }
     document.getElementById("nomesOrdem").innerHTML="Nome encontrado"+pesquisa+"vez(es)";
-    console.log("Nomes encontrado"+pesquisa+"vez(es)");
+    console.log("Nomes encontrado" + pesquisa + "vez(es)");
 }
